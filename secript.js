9777f6d669cd0b18be62622f396b73070a7acb57
shakhmuad <muradb356@gmail.com>
2023-07-13 16:01:01 +0400

    add responsiv

--- secript.js
+++ secript.js
@@ -1,6 +1,12 @@
 const menu = document.querySelector(".menu-icon");
 const nav = document.querySelector(".header-container-right");
+const select = document.querySelector(".select");
+const selectUl = document.querySelector(".select-ul");
 
 menu.addEventListener("click", function () {
   nav.classList.toggle("active");
+});
+
+selectUl.addEventListener("click", function () {
+  select.classList.toggle("open");
 });
