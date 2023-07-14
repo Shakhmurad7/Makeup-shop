d511aeb7c3d1dfd43da453e7cb011b6c52a79458
shakhmuad <muradb356@gmail.com>
2023-07-14 14:54:58 +0400

    add burger-menu

--- secript.js
+++ secript.js
@@ -2,11 +2,18 @@ const menu = document.querySelector(".menu-icon");
 const nav = document.querySelector(".header-container-right");
 const select = document.querySelector(".select");
 const selectUl = document.querySelector(".select-ul");
+const angleUP = document.querySelector(".angle-up");
+const angleDOWN = document.querySelector(".angle-down");
+const menuclose = document.querySelector(".menu-close");
 
 menu.addEventListener("click", function () {
   nav.classList.toggle("active");
 });
-
-selectUl.addEventListener("click", function () {
+menuclose.addEventListener("click", function () {
+  nav.classList.toggle("active");
+});
+select.addEventListener("click", function () {
   select.classList.toggle("open");
+  angleUP.classList.toggle("angle-up-none");
+  angleDOWN.classList.toggle("angle-down-inline");
 });
